3d221e0922d4cb50499abf65f66a9f8321283975
Sam Yan <yzenithcorp@gmail.com>
2018-06-13 15:28:12 -0500

    load quotes API

--- index.js
+++ index.js
@@ -7,9 +7,26 @@ function toggleHideBar() {
 	})
 }
 
+// quotes API section
+function getQuotesFromAPI(callback) {
+	const quotesApiEndPoint = 'https://talaikis.com/api/quotes/random/';
+	$.getJSON(quotesApiEndPoint,callback);
+}
+function showQuotes(quotes_Obj) {
+	console.log(quotes_Obj);
+	$('.quotes-area').html(
+		`<div class="quotes">
+			<q class="content">${quotes_Obj.quote}</q>
+			<span class="author"> <i class="fas fa-user"></i>${quotes_Obj.author}</span>
+			<button type="submit">Next <i class="fas fa-arrow-right"></i></button>
+		</div>`
+		);
+}
+
 
 function loadfunctions() {
 	toggleHideBar();
+	getQuotesFromAPI(showQuotes);
 }
 
 $(loadfunctions);
